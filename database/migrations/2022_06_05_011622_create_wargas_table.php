5bb60caf2ae96c93a59f6a10f0851f0220085437
Isep Lutpi Nur <Iseplutpinur7@gmail.com>
2022-06-06 03:50:29 +0700

    update db warga

--- database/migrations/2022_06_05_011622_create_wargas_table.php
+++ database/migrations/2022_06_05_011622_create_wargas_table.php
@@ -16,21 +16,35 @@ return new class extends Migration
         Schema::create('wargas', function (Blueprint $table) {
             $table->id();
             $table->string('nik', 16)->nullable()->default(null);
+
+            // data master
             $table->bigInteger('agama_id', false, true)->nullable()->default(null);
             $table->bigInteger('pendidikan_id', false, true)->nullable()->default(null);
             $table->bigInteger('pekerjaan_id', false, true)->nullable()->default(null);
             $table->bigInteger('status_kawin_id', false, true)->nullable()->default(null);
             $table->bigInteger('status_penduduk_id', false, true)->nullable()->default(null);
             $table->bigInteger('rt_id', false, true)->nullable()->default(null);
+
             $table->string('nama')->nullable()->default(null);
             $table->string('kota_lahir')->nullable()->default(null);
-            $table->date('tanggal_lahir')->nullable()->default(null);
             $table->string('jenis_kelamin')->nullable()->default(null);
             $table->boolean('ada_ktp')->nullable()->default(null);
             $table->boolean('ada_akte')->nullable()->default(null);
             $table->string('file_ktp')->nullable()->default(null);
             $table->string('file_akte')->nullable()->default(null);
             $table->text('alamat_lengkap')->nullable()->default(null);
+
+            $table->date('tanggal_lahir')->nullable()->default(null);
+            $table->date('tanggal_mati')->nullable()->default(null);
+            $table->date('tanggal_pindah')->nullable()->default(null);
+            $table->date('tanggal_datang')->nullable()->default(null);
+
+            $table->boolean('status_tinggal')->nullable()->default(1)->comment('1 lahir, 2 mati, 3 pindah, 4 datang');
+            $table->boolean('status')->nullable()->default(1)->comment('0 bukan warga setempat, 1 warga setempat');
+
+            $table->boolean('warga_negara')->nullable()->default(1)->comment('0 wna, 1 wni');
+            $table->string('negara_asal')->nullable()->default(null);
+
             $table->timestamps();
 
             $table->foreign('agama_id')
